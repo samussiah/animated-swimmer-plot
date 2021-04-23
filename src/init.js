d79b7fa8afd324f5b3c413d069e0b78a46f9fd41
Spencer Childress <samussiah@gmail.com>
2021-04-23 18:47:48 -0400

    did not save init

--- src/init.js
+++ src/init.js
@@ -1,8 +1,10 @@
 import set from './init/set';
 import scale from './init/scale';
+import axis from './init/axis';
 
 export default function init() {
     this.set = set.call(this);
     this.settings.barSize = this.settings.dimensions.height / this.set.id.size;
     this.scale = scale.call(this);
+    this.axis = axis.call(this);
 }
